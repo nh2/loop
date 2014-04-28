90adf3f402a7ac4e882452c745519163f6170071
Niklas Hambüchen <mail@nh2.me>
2014-04-28 01:12:46 +0100

    Switch from INLINABLE to INLINE, remove specializations.
    It looks to me that this should always be inlined.
    
    Does not affect benchmarking outcomes, apart from
    `unsafeLoopMonadIntInlinable` whose (anyway bad) time is improved
    by factor two (but we don't care about it).

--- src/Control/Loop.hs
+++ src/Control/Loop.hs
@@ -8,10 +8,6 @@ module Control.Loop
   , unsafeLoop
   ) where
 
-import Control.Monad.ST -- For SPECIALIZE only
-import Data.Int         -- For SPECIALIZE only
-import Data.Word        -- For SPECIALIZE only
-
 
 -- | @loop start end f@: Loops from @start@ to @end@ (inclusive), executing @f@
 -- on each iteration. Same as @forM_ [start..end] f@.
@@ -23,7 +19,7 @@ loop start end f = go start
     go !x | x == end  = f x
           | otherwise = f x >> go (succ x)
 
-{-# INLINEABLE loop #-}
+{-# INLINE loop #-}
 
 
 -- | Like `loop`, but without bounds (overflow) check.
@@ -38,27 +34,4 @@ unsafeLoop start end f = go start
           | otherwise = f x >> go (unsafeSucc x)
     unsafeSucc = toEnum . (+ 1) . fromEnum
 
-{-# INLINEABLE unsafeLoop #-}
-
-
--- SPECIALIZE for the monad
-{-# SPECIALIZE loop :: (Enum e, Eq e) => e -> e -> (e -> IO ()) -> IO () #-}
-{-# SPECIALIZE loop :: (Enum e, Eq e) => e -> e -> (e -> ST s ()) -> ST s () #-}
-
--- SPECIALIZE for what we iterate over
-{-# SPECIALISE loop :: (Monad m) => Bool     -> Bool     -> (Bool     -> m ()) -> m () #-}
-{-# SPECIALISE loop :: (Monad m) => Char     -> Char     -> (Char     -> m ()) -> m () #-}
-{-# SPECIALISE loop :: (Monad m) => Double   -> Double   -> (Double   -> m ()) -> m () #-}
-{-# SPECIALISE loop :: (Monad m) => Float    -> Float    -> (Float    -> m ()) -> m () #-}
-{-# SPECIALISE loop :: (Monad m) => Int      -> Int      -> (Int      -> m ()) -> m () #-}
-{-# SPECIALISE loop :: (Monad m) => Int8     -> Int8     -> (Int8     -> m ()) -> m () #-}
-{-# SPECIALISE loop :: (Monad m) => Int16    -> Int16    -> (Int16    -> m ()) -> m () #-}
-{-# SPECIALISE loop :: (Monad m) => Int32    -> Int32    -> (Int32    -> m ()) -> m () #-}
-{-# SPECIALISE loop :: (Monad m) => Int64    -> Int64    -> (Int64    -> m ()) -> m () #-}
-{-# SPECIALISE loop :: (Monad m) => Integer  -> Integer  -> (Integer  -> m ()) -> m () #-}
-{-# SPECIALISE loop :: (Monad m) => Ordering -> Ordering -> (Ordering -> m ()) -> m () #-}
-{-# SPECIALISE loop :: (Monad m) => Word     -> Word     -> (Word     -> m ()) -> m () #-}
-{-# SPECIALISE loop :: (Monad m) => Word8    -> Word8    -> (Word8    -> m ()) -> m () #-}
-{-# SPECIALISE loop :: (Monad m) => Word16   -> Word16   -> (Word16   -> m ()) -> m () #-}
-{-# SPECIALISE loop :: (Monad m) => Word32   -> Word32   -> (Word32   -> m ()) -> m () #-}
-{-# SPECIALISE loop :: (Monad m) => Word64   -> Word64   -> (Word64   -> m ()) -> m () #-}
+{-# INLINE unsafeLoop #-}
